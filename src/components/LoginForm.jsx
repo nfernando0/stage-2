5b34a588dd86cbcfb3e5d4818a18740119c2b274
nfernando0 <nandof986@gmail.com>
2023-06-26 20:23:48 +0700

    Register Form

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -11,7 +11,7 @@ function LoginForm() {
                     <Form>
                         <Form.Group className="mb-3" controlId="username">
                             <Form.Label>Username</Form.Label>
-                            <Form.Control type="text" placeholder="Enter username" />
+                            <Form.Control type="text" placeholder="Username" />
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="password">
                             <Form.Label>Password</Form.Label>
